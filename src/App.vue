60e239adc7cc5c69c6076b46c13b48032a01bf6a
FelixJacobsen <felix.jacobsen@iths.se>
2022-09-11 20:40:59 +0200

    Add header with NavHeader component

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>  
+    <NavHeader title="Testie" />
+</template>
+
+<script>
+import NavHeader from "./components/NavHeader.vue";
+
+export default {
+    name: "App",
+    components: {
+        NavHeader,
+    },
+};
+</script>
---
+++ src/components/NavHeader.vue
@@ -0,0 +1,49 @@
+<template>
+        <header>
+            <img class="nav-logo" src="../assets/img/logo.png" alt="logo"> 
+        <div class="nav-container">
+                <uL class="nav-list">
+                    <router-link class="nav-item">Home</router-link>
+                    <router-link class="nav-item">Workouts</router-link>
+                    <router-link class="nav-item">About</router-link>
+                </uL>
+        </div>
+    </header>
+</template>
+
+
+<script>
+export default {
+    name: "NavHeader",
+}
+</script>
+
+<style scoped>
+
+
+.nav-container{
+    padding: 5px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: space-between;
+    background: green;
+}
+
+
+
+.nav-logo{
+    position: absolute;
+    width: 10rem;
+    height: 10rem;
+}
+
+.nav-list{
+}
+
+.nav-item{
+
+}
+
+</style>
+
