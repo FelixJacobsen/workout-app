f02f43ce69b28cad25936a56c77d31056ee01553
FelixJacobsen <felix.jacobsen@iths.se>
2022-10-22 12:49:05 +0200

    Styled displaying a cardio workout

--- src/components/WorkoutListItem.vue
+++ src/components/WorkoutListItem.vue
@@ -1,29 +1,46 @@
-<template
-  class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6"
->
-  <img
-    v-if="workout.type === 'cardio'"
-    class="h-20 w-auto px-10"
-    src="../assets/img/cardio.png"
-  />
+<template>
+  <div class="container mt-20">
+    <div class="flex flex-col items-center bg-gray-200 p-8 shadow-md">
+      <img
+        v-if="workout.type === 'cardio'"
+        class="h-20 w-auto px-10"
+        src="../assets/img/cardio.png"
+      />
 
-  <img v-else class="h-16 w-auto px-10" src="../assets/img/strength.png" />
+      <img v-else class="h-16 w-auto px-10" src="../assets/img/strength.png" />
 
-  <button @click="$emit('removeWorkout', workout.id)">Delete</button>
-  <p class="mt-6 py-1 px-8 text text-white bg-light-green shadow-md rounded-lg">
-    Workout type: {{ workout.type }}
-  </p>
-  <h1 class="mt-8 mb-2 text-center text-xl text-light-green">
-    {{ workout.name }}
-  </h1>
+      <button class="text-red-900" @click="$emit('removeWorkout', workout.id)">
+        Delete workout
+      </button>
+      <p
+        class="mt-6 py-1 px-8 text text-white bg-light-green shadow-md rounded-lg uppercase"
+      >
+        {{ workout.type }}
+      </p>
+      <h1 class="mt-8 mb-2 text-center text-xl text-light-green">
+        Workout name: {{ workout.name }}
+      </h1>
 
-  <h1 class="mt-8 mb-2 text-center text-xl text-light-green">
-    <ul>
-      <li v-for="exercise in workout.exercises">
-        <p>{{ exercise.duration }}</p>
-      </li>
-    </ul>
-  </h1>
+      <h1 class="mt-8 mb-2 text-center text-xl text-light-green">
+        <ul>
+          <li v-for="exercise in workout.exercises">
+            <p>Type of exercise: {{ exercise.cardioType }}</p>
+            <p>Distance: {{ exercise.distance }}</p>
+            <p>Duration: {{ exercise.duration }}</p>
+          </li>
+        </ul>
+      </h1>
+      <h1 class="mt-8 mb-2 text-center text-xl text-light-green">
+        <ul>
+          <li v-for="exercise in workout.exercises">
+            <p>Type of exercise: {{ exercise.weight }}</p>
+            <p>Distance: {{ exercise.sets }}</p>
+            <p>Duration: {{ exercise.reps }}</p>
+          </li>
+        </ul>
+      </h1>
+    </div>
+  </div>
 </template>
 
 <script setup>
